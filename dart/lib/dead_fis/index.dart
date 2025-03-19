95e184efcc97adf377bafe2097935464265881e9
deadlyunicorn <admin@deadlyunicorn.dev>
2025-03-19 10:06:24 +0200

    added dead fish

--- dart/lib/dead_fis/index.dart
+++ dart/lib/dead_fis/index.dart
@@ -1,3 +1,24 @@
 List<int> parse(String data) {
-  return [];
+  final List<int> result = <int>[];
+
+  for (int accumulator = 0, charIndex = 0;
+      charIndex < data.length;
+      charIndex++) {
+    final String currentChar = data[charIndex];
+    switch (currentChar) {
+      case "i":
+        accumulator += 1;
+      case "d":
+        accumulator -= 1;
+      case "s":
+        accumulator *= accumulator;
+      case "o":
+        result.add(accumulator);
+
+      default:
+        continue;
+    }
+  }
+
+  return result;
 }
